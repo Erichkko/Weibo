a7dd91d9c9696630ecb2c1c0bf288a9d35b1eaca
erichkko <erichkko@163.com>
2016-08-21 22:32:42 +0800

    update plusVc
    删除加好控制器无用的代码

--- Weibo/Weibo/Classes/Main/Resource/NullViewController.swift
+++ Weibo/Weibo/Classes/Main/Resource/NullViewController.swift
@@ -4,7 +4,9 @@
 //
 //  Created by 李南江 on 15/8/31.
 //  Copyright © 2015年 xiaomage. All rights reserved.
-//
+
+
+//  为plusBtn设置的占位ViewController
 
 import UIKit
 
@@ -13,23 +15,6 @@ class NullViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
 }
