68b2f38651feb4641f071f01a410a917d75ebc5d
erichkko <erichkko@163.com>
2016-08-21 20:21:08 +0800

    动态加载命名空间
    1.动态加载命名空间
    2.通过命名空间创建对象

--- Weibo/Weibo/Classes/Main/MainViewController.swift
+++ Weibo/Weibo/Classes/Main/MainViewController.swift
@@ -15,37 +15,70 @@ class MainViewController: UITabBarController {
         
         //设置tabbar属性
         setupTabbar()
+//        
+//        //1.创建首页
+//        let homeVc = HomeViewController()
+//        addChildViewController(homeVc, title: "首页", normalImage: "tabbar_home", selectImage: "tabbar_home_highlighted")
+//        
+//        //2.创建Message
+//        let messageVc = MessageViewController()
+//        addChildViewController(messageVc, title: "消息", normalImage: "tabbar_message_center", selectImage: "tabbar_message_center_highlighted")
+//        
+//        //4.创建Discover
+//        let discoverVc = DiscoverViewController()
+//        addChildViewController(discoverVc, title: "发现", normalImage: "tabbar_discover", selectImage: "tabbar_discover_highlighted")
+//        
+//        //5.创建ProFile
+//        let profileVc = ProfileViewController()
+//        addChildViewController(profileVc, title: "我", normalImage: "tabbar_profile", selectImage: "tabbar_profile_highlighted")
         
         //1.创建首页
-        let homeVc = HomeViewController()
-        addChildViewController(homeVc, title: "首页", normalImage: "tabbar_home", selectImage: "tabbar_home_highlighted")
+        addChildViewController("HomeViewController", title: "首页", normalImage: "tabbar_home", selectImage: "tabbar_home_highlighted")
         
         //2.创建Message
-        let messageVc = MessageViewController()
-        addChildViewController(messageVc, title: "消息", normalImage: "tabbar_message_center", selectImage: "tabbar_message_center_highlighted")
+        addChildViewController("MessageViewController", title: "消息", normalImage: "tabbar_message_center", selectImage: "tabbar_message_center_highlighted")
         
         //4.创建Discover
-        let discoverVc = DiscoverViewController()
-        addChildViewController(discoverVc, title: "发现", normalImage: "tabbar_discover", selectImage: "tabbar_discover_highlighted")
+        addChildViewController("DiscoverViewController", title: "发现", normalImage: "tabbar_discover", selectImage: "tabbar_discover_highlighted")
         
         //5.创建ProFile
-        let profileVc = ProfileViewController()
-        addChildViewController(profileVc, title: "我", normalImage: "tabbar_profile", selectImage: "tabbar_profile_highlighted")
+        addChildViewController("ProfileViewController", title: "我", normalImage: "tabbar_profile", selectImage: "tabbar_profile_highlighted")
     
     }
     
     private func setupTabbar(){
         self.tabBar.tintColor = UIColor.orangeColor()
     }
-    /**
-     添加Tabbar 自控制器
-     
-     - parameter childVc:     自控制器
-     - parameter title:       标题
-     - parameter normalImage: 默认图片
-     - parameter selectImage: 高亮图片
-     */
-    private func addChildViewController(childVc: UIViewController,title:String,normalImage:String,selectImage:String) {
+//    /**
+//     添加Tabbar 自控制器
+//     
+//     - parameter childVc:     自控制器
+//     - parameter title:       标题
+//     - parameter normalImage: 默认图片
+//     - parameter selectImage: 高亮图片
+//     */
+//    private func addChildViewController(childVc: UIViewController,title:String,normalImage:String,selectImage:String) {
+//        //抽取方法
+//        childVc.title = title
+//        childVc.tabBarItem.image = UIImage(named: normalImage)
+//        childVc.tabBarItem.selectedImage = UIImage(named: selectImage)
+//        let nav = MainNavViewController(rootViewController: childVc)
+//        self.addChildViewController(nav)
+//    }
+    
+    private func addChildViewController(childVcName: String,title:String,normalImage:String,selectImage:String) {
+        
+        
+        //动态获得命名空间
+        let targetName = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        //获得对应类的命名空间
+        let cls:AnyClass? = NSClassFromString(targetName + "." + childVcName)
+        //把AnyClass 强制转换 为ViewController
+        let vcCls = cls as! UIViewController.Type
+        //创建对象
+        let childVc = vcCls.init()
+    
+        
         //抽取方法
         childVc.title = title
         childVc.tabBarItem.image = UIImage(named: normalImage)
